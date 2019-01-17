3bc1039f967b2ae183a8f0f83d2fdec4cee0e438
Pierre Nallet <pierrenallet@hotmail.com>
2019-01-17 11:13:42 -0500

    Add project files.

---
+++ MyAzureWebApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyAzureWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var account = CloudStorageAccount.Parse(@"DefaultEndpointsProtocol=https;AccountName=pierrestoragetest;AccountKey=8OBdp85+f+p3zZOJEqPKeO7BUCV42gJ7MqUhiWCpYheOnlr92Ag3SatcrALHmRPg2LvsKXbWYqv7J/x3dRbJTQ==;EndpointSuffix=core.windows.net");
+            var container = account.CreateCloudBlobClient().GetContainerReference("cats");
+            var blobs = container.ListBlobs().OfType<CloudBlockBlob>().ToList();
+            return View(blobs);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
